3e8b9b6156079286689ee6eed202e35913a80cf4
elibapbap <22-01497@g.batstate-u.edu.ph>
2026-02-06 23:30:30 +0800

    Added Spring Configuration

---
+++ src/test/java/com/github/lovely/Book_Inventory/BookInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lovely.Book_Inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
